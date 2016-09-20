360ba210117c6cbffedeaf6771e9a575793042c3
Tyler Scott <tybugman@hotmail.com>
2016-09-20 10:19:06 -0600

    modularized the things

--- iris.py
+++ iris.py
@@ -4,84 +4,52 @@ Created on Sat Sep 17 17:55:00 2016
 
 @author: Tyler Scott
 """
+import sys
+from load import Dataset_Loader as dl
+from classifiers.hardcoded import Hard_Coded as hc
 
 
-from sklearn import datasets
-from random import shuffle
-from numpy.random import permutation
-from numpy import split
-from copy import deepcopy as dc
+def getInput():
+    location = input("If you are loading a csv please input the filename, otherwise input the name of the dataset: ") or 'iris'
+    opts = input("Enter loading options separated by commas (options available = shuffled, split): ") or 'shuffled'
+    split_amount = 0.0
+    if 'split' in opts:
+        split_amount = input("Enter the split percentage as a decimal (default is 0.7): ") or 0.7
+    print("loading dataset @ " + location + " with " + opts +" enabled...")
+    return location, opts, split_amount
 
-iris = datasets.load_iris()
-def shuffle_dataset(dataset):
-    data = dataset.data
-    target = dataset.target
-    zipData = list(zip(data, target))
-    shuffle(zipData)
-    data[:], target[:] = zip(*zipData)
-    dataset.data = data
-    dataset.target = target
-    return dataset    
-    
-def split_dataset(dataset):
-    data = dataset.data
-    target = dataset.target
-    train = dc(dataset)
-    predict = dc(dataset)
-    predict.data, train.data=data[:len(data)//3],data[len(data)//3:]
-    predict.target, train.target=target[:len(target)//3],target[len(target)//3:]
-#    print("trainData length:", len(train.data))
-#    print("trainTarget length:", len(train.target))
-#    print("predictData length:", len(predict.data))
-#    print("predictTarget length:", len(predict.target))
-    return train, predict
+def process_data(datas):
+    # for i in range(len(datasets)):
+    #     for j in range(len(datasets[i].target)):
+    #         print("j ", datasets[i].target[j])0
+    hc.train(datas[0].data, datas[0].target)
+    hc.predict(datas[1].data, datas[1].target)
 
-def train(dataset):
-    if dataset: 
-        return True
-    else:
-        return False
+def main(args):
+    location, opts, split_amount = getInput()
+    print("SPLIT AMT:", split_amount)
 
-def predict(dataset):
-    if dataset:
-        return True
-    else:
-        return False
+    dataset = dl.load_dataset(location, opts)
+    training_dataset, testing_dataset = dl.split_dataset(dataset, split_amount)
 
-shuffled_dataset = shuffle_dataset(iris)
-training_dataset, predicting_dataset = split_dataset(shuffled_dataset)
+    # print("dataset length:", len(dataset))
+    # print("training_dataset length:", len(training_dataset))
+    # print("testing_dataset length:", len(testing_dataset))
+    #
+    # print("training_dataset.data:", training_dataset.data)
+    # print("training_dataset.target:", training_dataset.target)
+    # print("training_dataset.target_names:", training_dataset.target_names)
+    #
+    # print("testing_dataset.data:", testing_dataset.data)
+    # print("testing_dataset.target:", testing_dataset.target)
+    # print("testing_dataset.target_names:", testing_dataset.target_names)
+    #
+    # print("train_dataset.data length:", len(training_dataset.data))
+    # print("train_dataset.target length:", len(training_dataset.target))
+    # print("testing_dataset.data length:", len(testing_dataset.data))
+    # print("testing_dataset.target length:", len(testing_dataset.target))
+    process_data([training_dataset, testing_dataset])
+    hc.printAll(dataset)
 
-
-print("iris length:", len(iris))
-print("shuffled_dataset length:", len(shuffled_dataset))
-print("training_dataset length:", len(training_dataset))
-print("predicting_dataset length:", len(predicting_dataset))
-
-print("training_dataset.data:\n", training_dataset.data)
-print("training_dataset.target:\n", training_dataset.target)
-print("training_dataset.target_names:\n", training_dataset.target_names)
-
-print("predicting_dataset.data:\n", predicting_dataset.data)
-print("predicting_dataset.target:\n", predicting_dataset.target)
-print("predicting_dataset.target_names:\n", predicting_dataset.target_names)
-
-print("train_dataset.data length:", len(training_dataset.data))
-print("train_dataset.target length:", len(training_dataset.target))
-print("predicting_dataset.data length:", len(predicting_dataset.data))
-print("predicting_dataset.target length:", len(predicting_dataset.target))
-
-print(train(training_dataset))
-print(predict(predicting_dataset))
-
-def parse_dataset(dataset):
-    newData = {}
-    
-    for i in range(len(dataset.data)):
-        newData.append({
-            "index" : i,
-            "sepal_length": dataset.data[i][0],
-            "sepal_width": dataset.data[i][1],
-            "petal_length": dataset.data[i][2],
-            "petal_width": dataset.data[i][3],    
-        })
-    return newData
+if __name__ == "__main__":
+    main(sys.argv)
